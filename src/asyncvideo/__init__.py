bebab14c7cdca70b11b07bb4bb161c23e68a6e99
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2026-07-29 17:26:36 -0400

    add deploy action and scm for auto discovery version

--- src/asyncvideo/__init__.py
+++ src/asyncvideo/__init__.py
@@ -1,5 +1,19 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from ._pyav_video_reader import VideoHandler, pyav_trim_plane
 from .convert import to_rgb
 from .vr_async import AsyncVideoReader
 
-__all__ = ["AsyncVideoReader", "VideoHandler", "pyav_trim_plane", "to_rgb"]
+try:
+    # set at build time by setuptools_scm, from the git tag
+    __version__ = version("asyncvideo")
+except PackageNotFoundError:  # pragma: no cover - running from a source tree
+    __version__ = "unknown"
+
+__all__ = [
+    "AsyncVideoReader",
+    "VideoHandler",
+    "__version__",
+    "pyav_trim_plane",
+    "to_rgb",
+]
